fa21ba4ce95141c7361e4214521730360fc28406
Fidelia Matos <fideliamatos@gmail.com>
2020-06-25 01:08:53 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.rbtnX = new System.Windows.Forms.RadioButton();
+            this.rbtn0 = new System.Windows.Forms.RadioButton();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.lblTicTacToe = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNotAnyWinner = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn00.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn00.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn00.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn00.ForeColor = System.Drawing.Color.Black;
+            this.btn00.Location = new System.Drawing.Point(149, 184);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(104, 84);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.btn00_Click);
+            // 
+            // rbtnX
+            // 
+            this.rbtnX.AutoSize = true;
+            this.rbtnX.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbtnX.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnX.Location = new System.Drawing.Point(502, 184);
+            this.rbtnX.Name = "rbtnX";
+            this.rbtnX.Size = new System.Drawing.Size(87, 21);
+            this.rbtnX.TabIndex = 1;
+            this.rbtnX.TabStop = true;
+            this.rbtnX.Text = "Player X";
+            this.rbtnX.UseVisualStyleBackColor = true;
+            // 
+            // rbtn0
+            // 
+            this.rbtn0.AutoSize = true;
+            this.rbtn0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbtn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn0.Location = new System.Drawing.Point(502, 220);
+            this.rbtn0.Name = "rbtn0";
+            this.rbtn0.Size = new System.Drawing.Size(86, 21);
+            this.rbtn0.TabIndex = 2;
+            this.rbtn0.TabStop = true;
+            this.rbtn0.Text = "Player 0";
+            this.rbtn0.UseVisualStyleBackColor = true;
+            // 
+            // btn02
+            // 
+            this.btn02.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn02.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn02.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn02.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.Location = new System.Drawing.Point(357, 184);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(104, 84);
+            this.btn02.TabIndex = 3;
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.btn02_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn01.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn01.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn01.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.Location = new System.Drawing.Point(253, 184);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(104, 84);
+            this.btn01.TabIndex = 4;
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.btn01_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn11.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn11.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(253, 268);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(104, 84);
+            this.btn11.TabIndex = 7;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn12.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn12.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(357, 268);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(104, 84);
+            this.btn12.TabIndex = 6;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn10.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.Location = new System.Drawing.Point(149, 268);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(104, 84);
+            this.btn10.TabIndex = 5;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn21.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn21.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(253, 352);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(104, 84);
+            this.btn21.TabIndex = 10;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.btn21_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn22.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn22.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(357, 352);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(104, 84);
+            this.btn22.TabIndex = 9;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.btn22_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn20.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn20.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Honeydew;
+            this.btn20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn20.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn20.Location = new System.Drawing.Point(149, 352);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(104, 84);
+            this.btn20.TabIndex = 8;
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.btn20_Click);
+            // 
+            // lblTicTacToe
+            // 
+            this.lblTicTacToe.AutoSize = true;
+            this.lblTicTacToe.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTicTacToe.ForeColor = System.Drawing.Color.Red;
+            this.lblTicTacToe.Location = new System.Drawing.Point(224, 106);
+            this.lblTicTacToe.Name = "lblTicTacToe";
+            this.lblTicTacToe.Size = new System.Drawing.Size(173, 31);
+            this.lblTicTacToe.TabIndex = 12;
+            this.lblTicTacToe.Text = "Tic-Tac-Toe";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnReset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnReset.Location = new System.Drawing.Point(386, 486);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNotAnyWinner
+            // 
+            this.lblNotAnyWinner.AutoSize = true;
+            this.lblNotAnyWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotAnyWinner.ForeColor = System.Drawing.Color.Red;
+            this.lblNotAnyWinner.Location = new System.Drawing.Point(144, 482);
+            this.lblNotAnyWinner.Name = "lblNotAnyWinner";
+            this.lblNotAnyWinner.Size = new System.Drawing.Size(215, 26);
+            this.lblNotAnyWinner.TabIndex = 14;
+            this.lblNotAnyWinner.Text = "Not any winner   →";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 570);
+            this.Controls.Add(this.lblNotAnyWinner);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblTicTacToe);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.rbtn0);
+            this.Controls.Add(this.rbtnX);
+            this.Controls.Add(this.btn00);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "    ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.RadioButton rbtnX;
+        private System.Windows.Forms.RadioButton rbtn0;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Label lblTicTacToe;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNotAnyWinner;
+    }
+}
+
